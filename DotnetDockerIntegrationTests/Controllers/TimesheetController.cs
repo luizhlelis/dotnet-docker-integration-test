962fa09b8b84f1d474a8216dc365097a458dac1d
Alvaro Kramer <alvaro.kramer@avenuecode.com>
2020-11-20 14:17:52 -0300

    First changes.

---
+++ DotnetDockerIntegrationTests/Controllers/TimesheetController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DotnetDockerIntegrationTests.Controllers
+{  
+    public class TimesheetController : ApiController
+    {
+
+
+        public TimesheetController()
+        {
+                
+        }
+    }
+
+}
---
+++ DotnetDockerIntegrationTests/Models/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotnetDockerIntegrationTests.Models
+{
+    public class Entity
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+    }
+
+    public class Employee : Entity
+    {
+        public string Name { get; set; }
+        public string Registration { get; set; }
+        public virtual ICollection<Project> Projects { get; set; }
+        public virtual ICollection<TimeEntry> TimeEntries { get; set; }
+    }
+
+    public class Project : Entity
+    {
+        public string Name { get; set; }
+        public virtual ICollection<Employee> Employees { get; set; }
+        public virtual ICollection<TimeEntry> TimeEntries { get; set; }
+    }
+
+    public class TimeEntry : Entity
+    {
+        public DateTime Date { get; set; }
+        public string Start { get; set; }
+        public string End { get; set; }
+        public virtual Employee Employee { get; set; }
+        public virtual Project Project { get; set; }
+    }
+}
