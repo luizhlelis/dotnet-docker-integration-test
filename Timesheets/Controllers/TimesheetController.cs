80b177dcf86d3483c75b5ed036ca65236156a46e
Stefano Bretas <stefano.pietro@avenuecode.com>
2020-11-20 15:38:51 -0300

    feature: implements controller methods

--- Timesheets/Controllers/TimesheetController.cs
+++ Timesheets/Controllers/TimesheetController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Timesheets;
 using Timesheets.Models;
@@ -21,25 +22,41 @@ namespace DotnetDockerIntegrationTests.Controllers
         [HttpGet]
         public async Task<IActionResult> GetTimesheetsByEmployeeAsync(Guid employeeId)
         {
-            throw new NotImplementedException();
+            var timesheets = await _dbContext.TimeEntries.Where(x => x.Employee.Id == employeeId).ToListAsync();
+
+            if (timesheets is null || timesheets.Count == 0)
+                return NoContent();
+
+            return Ok(timesheets);
         }
 
         [HttpPost]
         public async Task<IActionResult> InsertTimeSheetAsync(TimeEntry timeEntry)
         {
-            throw new NotImplementedException();
+            await _dbContext.AddAsync(timeEntry);
+            await _dbContext.SaveChangesAsync();
+
+            return Created("time entry", timeEntry);
         }
 
         [HttpPut]
-        public async Task<IActionResult> UpdateTimeEntryAsync(Guid timeEntryId, TimeEntry timeEntry)
+        public async Task<IActionResult> UpdateTimeEntryAsync(TimeEntry timeEntry)
         {
-            throw new NotImplementedException();
+            var updated = _dbContext.Update(timeEntry);
+            updated.State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+
+            return Ok(updated.Entity);
         }
 
         [HttpDelete]
         public async Task<IActionResult> DeleteTimeEntryAsync(Guid timeEntryId)
         {
-            throw new NotImplementedException();
+            var timeEntry = _dbContext.TimeEntries.FirstOrDefault(x => x.Id == timeEntryId);
+            _dbContext.TimeEntries.Remove(timeEntry);
+            await _dbContext.SaveChangesAsync();
+
+            return Ok();
         }
     }
 
